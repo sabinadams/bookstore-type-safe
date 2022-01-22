dc18644dec714fcd2843595693555de49e0daea7
Sabin Adams <sabintheworld@gmail.com>
2022-01-21 20:28:03 -0800

    Simplifies and adds some best-practices

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -9,24 +9,22 @@ export function App() {
   useEffect(() => {
     fetch('http://localhost:3333/api')
       .then( result => result.json())
-      .then( data => {
-        setBooks(data)
-      })
+      .then( setBooks )
   }, [])
 
-  return (
-    <>
-      {
-        books.map( book => (
-          <div style={{ border: '1px solid black', padding: '2rem'}}>
-            <p>{book.id}</p>
-            <p>{book.title}</p>
-            <p>{book.authorName}</p>
-          </div>
-        ))
-      }
-    </>
-  );
+return (
+  <>
+    {
+      books.map( book => (
+        <div key={book.id} style={{ border: '1px solid black', padding: '2rem'}}>
+          <p>{book.id}</p>
+          <p>{book.title}</p>
+          <p>{book.authorName}</p>
+        </div>
+      ))
+    }
+  </>
+);
 }
 
 export default App;
